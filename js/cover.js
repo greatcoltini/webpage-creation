dccb5d336a3ebc65578bd98ab2dc97b229422e03
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-09-05 00:10:33 -0400

    -- wip for cycle cover image on time of day

--- js/cover.js
+++ js/cover.js
@@ -1,12 +1,18 @@
-
-
 var cover_choices = new Array('assets/cover_small.jpg', 'assets/cover_small_dark.jpg');
+var el = document.getElementById("cover");
+const hours = new Date().getHours()
+const isDayTime = hours > 6 && hours < 20
 
-function randomCover() {
-	var rand = Math.floor(Math.random() * cover_choices.length);
-	$('body').css({
-		'background-image' : 'url(' + cover_choices[rand] +')'
-	});
+function daytime_cover() {
+	var width = document.getElementById("cover").clientWidth;
+	if (isDayTime == false && width < 800) {
+		el.style.background = "url(assets/cover_small_dark.jpg)";
+		el.className = "d-flex h-100 text-center text-bg-dark bd-placeholder-img";
+		el.style.backgroundSize = "100%";
+	}
 }
 
-window.onload = randomCover;
+window.onload = daytime_cover();
+
+
+document.getElementsByTagName("body")[0].onresize = daytime_cover();
